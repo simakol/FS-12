72222197c46af911c5e24d2f0cfb60818acf4099
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-06-13 18:51:18 +0300

    lesson-01 js

---
+++ JS/lesson-01/script.js
@@ -0,0 +1,69 @@
+/*
+console.log("Hello, world!");
+console.log(6);
+
+let apples = 10;
+
+console.log("На складі ", apples, " яблук");
+
+apples = 15;
+
+console.log("На складі ", apples, " яблук");
+
+// Data types
+
+let numbers = -10; // Numbers (98, -793, 0, 8.657)
+let string = "Hello, I am string!"; // String будь-яке значення у кавичках
+let boolean = false; // Boolean(bool) логічний тип, вміє зберігати тільки true(правда) і false(неправда)
+let nullType = null; // Null - нічого, або не існує (відсутність присутності)
+let undefinedType = undefined; // undefined - відсутність значення (присутність відсутності)
+
+// typeof - оператор для перевірки типу даних якогось значення
+
+let user = 8.905;
+
+console.log(typeof user);
+
+*/
+
+// let a = 5;
+// let b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** 5); // возведення в степінь
+// console.log(Math.sqrt(16)); // квадратний корінь
+// console.log(Math.pow(5, 3)); // возведення у степінь 5^3
+// console.log(Math.PI); // число пі
+// console.log(Math.cos(90));
+// console.log(Math.tan(90));
+// console.log(Math.sin(90));
+// console.log(Math.sin(Math.PI));
+// console.log(10 % 4); // Остача від ділення - найближча до 10 яка ділиться на 4 націло це 8. 10 - 8 = 2 - відповідь
+
+// console.log(15 % 3); // 0
+// console.log(11 % 3); // 2
+
+// console.log(10 % 2); // 0 - парне
+// console.log(19 % 2); // 1 - непарне
+
+// console.log(4 > 6);
+// console.log(5 < 10);
+// console.log(5 >= 5);
+// console.log(4 <= 5);
+
+// let c = "10"; // string
+// let d = 10; // number
+
+// console.log(c == d); // true -> "10" == "10" -> true рівність зі зведенням типів, дивиться на типи, якщо різні - приводить все до спільного і потім порівнює
+// console.log(c === d); // false - спочатку порівнює типи, якщо типи різні - повертає відразу false
+
+// let userName = prompt("Вітаю на сайті! Введіть своє імʼя"); //запитати у користувача
+
+// alert("Привіт, " + userName); // повідомлення в окремому модальному вікні
+
+// let ageStatus = confirm("Вам є 18 років?"); // вікно з питанням і кнопками OK(true) і Cancel(false)
+
+// alert(ageStatus);
