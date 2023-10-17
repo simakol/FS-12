4b672e4abdcb62cf1e127acd2cb828f35f7102bf
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-10-17 18:16:29 +0300

    start rick&morty app

--- advanced JS/fetch/script.js
+++ advanced JS/fetch/script.js
@@ -1,8 +1,32 @@
+const refs = {
+  count: document.getElementById("count"),
+  list: document.getElementById("list"),
+};
+
 getCharacters()
   .then((data) => {
+    count.textContent = data.info.count;
     console.log(data);
+    refs.list.insertAdjacentHTML("beforeend", createMarkup(data.results));
   })
   .catch((err) => console.log(err));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ id, name, gender, image, species, status }) => `
+  <li data-id="${id}">
+    <img src="${image}" alt="${name}">
+    <h2>${name}</h2>
+    <p>Gender: <span>${gender}</span></p>
+    <p>Species: <span>${species}</span></p>
+    <p>Status: <span>${status}</span></p>
+  </li>
+  
+  `
+    )
+    .join("");
+}
 
 function getCharacters() {
   const BASE_URL = "https://rickandmortyapi.com/api";
